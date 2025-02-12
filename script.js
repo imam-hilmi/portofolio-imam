3e346335b6e006842da89852c0da02b7e541662e
Imam Hilmi Saputra <imamhilmisaputra47@gmail.com>
2025-02-13 01:24:44 +0700

    script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // // Interaki (efek hover)
-// const proyek = document.querySelectorAll(".proyek");
+const proyek = document.querySelectorAll(".proyek");
 
-// proyek.forEach(proyekItem => { // proyekItem => menggunakan nama variabel yang berbeda di dalam foreach
-//     proyekItem.addEventListener('mouseover', () => {
-//         proyekItem.style.transform = 'scale(1.05)';
-//     }) ;
+proyek.forEach(proyekItem => { // proyekItem => menggunakan nama variabel yang berbeda di dalam foreach
+    proyekItem.addEventListener('mouseover', () => {
+        proyekItem.style.transform = 'scale(1.05)';
+    }) ;
 
-//     proyekItem.addEventListener('mouseout', () => {
-//         proyekItem.style.transform = 'scale(1)';
-//     });
-// });
+    proyekItem.addEventListener('mouseout', () => {
+        proyekItem.style.transform = 'scale(1)';
+    });
+});
 
 // Menampilkan konten yand disembunyikan menggunakan event listener
 document.getElementById('show-more-btn').addEventListener('click', function() {
